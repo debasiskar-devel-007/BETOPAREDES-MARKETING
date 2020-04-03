4d346fde8c4c4975a54c059d455987cb08973ffd
amitavainfluxiq <amitava.involutiontech@gmail.com>
2020-04-03 19:23:46 +0530

    amitava markating page

---
+++ src/app/productreview/productreview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productreview',
+  templateUrl: './productreview.component.html',
+  styleUrls: ['./productreview.component.css']
+})
+export class ProductreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
