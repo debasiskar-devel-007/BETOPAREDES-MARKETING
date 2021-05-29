4458dff4b3c143168b9b97fae3c453fa87434d49
Akash-influxiq <akash.influxiq@gmail.com>
2021-05-29 18:54:08 +0530

    uptodate

--- src/app/marketingreview-new/marketingreview-new.component.ts
+++ src/app/marketingreview-new/marketingreview-new.component.ts
@@ -8,21 +8,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class MarketingreviewNewComponent implements OnInit {
 
-  constructor(public api_service:ApiService,
-  public activatedroute: ActivatedRoute,) { }
+  constructor(public api_service: ApiService,
+    public activatedroute: ActivatedRoute,) { }
 
   ngOnInit() {
   }
-  request_contract(){
+  request_contract() {
     console.log(this.activatedroute.snapshot.params);
-    let endpoint='https://z2oo2a8oq9.execute-api.us-east-1.amazonaws.com/dev/api/requestcontractfromlead'
-let data={
-  product_id:this.activatedroute.snapshot.params.product_id,
-  lead_id:this.activatedroute.snapshot.params.lead_id,
-  rep_id:this.activatedroute.snapshot.params.rep_id
-}
-this.api_service.requestData(endpoint,data).subscribe((res:any)=>{
+    let endpoint = 'https://z2oo2a8oq9.execute-api.us-east-1.amazonaws.com/dev/api/requestcontractfromlead'
+    let data = {
+      product_id: this.activatedroute.snapshot.params.product_id,
+      lead_id: this.activatedroute.snapshot.params.lead_id,
+      rep_id: this.activatedroute.snapshot.params.rep_id
+    }
+    this.api_service.requestData(endpoint, data).subscribe((res: any) => {
 
-})
+    })
   }
 }
