6bd3ad9dcbe47a361fa2ca46ebe2a166877a8bc9
amitavainfluxiq <amitava.involutiontech@gmail.com>
2021-12-29 11:11:10 +0530

    amitava

--- src/app/marketingreview-new/marketingreview-new.component.ts
+++ src/app/marketingreview-new/marketingreview-new.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../api.service";
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
+import { log } from 'console';
 @Component({
   selector: 'app-marketingreview-new',
   templateUrl: './marketingreview-new.component.html',
@@ -9,23 +10,28 @@ import { MatSnackBar } from '@angular/material';
 })
 export class MarketingreviewNewComponent implements OnInit {
   public requestflag: boolean = false;
-  public productdata:any;
-  public techata:any;
+  public productdata: any;
+  public techata: any;
+  public videolist: any = [{ name: 'Sale deck', videodata: [{ url: 'https://betoparedesallvideos.s3.amazonaws.com/betoparedesallvideos/image-16375886325607774485510954222850.mp4', title: 'TM-FLOW REPORT', description: ' TM-FLOW REPORT. VASCULAR FUNCTION ASSESSMENT. HR: 77. Height 5 ,. Weight 168 Lbs. BMI: 26.3. CLINICAL CONTEXT. Physician Name:.' }, { url: 'https://betoparedesallvideos.s3.amazonaws.com/betoparedesallvideos/image-1637210549150PECEDoctorTrainingVideo.mp4', title: 'TM-FLOW REPORT', description: ' TM-FLOW REPORT. VASCULAR FUNCTION ASSESSMENT. HR: 77. Height 5 ,. Weight 168 Lbs. BMI: 26.3. CLINICAL CONTEXT. Physician Name:.' }] }, { name: 'Sales pitch', videodata: [{ url: 'https://betoparedesallvideos.s3.amazonaws.com/betoparedesallvideos/image-16375886325607774485510954222850.mp4', title: 'TM-FLOW REPORT', description: ' TM-FLOW REPORT. VASCULAR FUNCTION ASSESSMENT. HR: 77. Height 5 ,. Weight 168 Lbs. BMI: 26.3. CLINICAL CONTEXT. Physician Name:.' }, { url: 'https://betoparedesallvideos.s3.amazonaws.com/betoparedesallvideos/image-1637210549150PECEDoctorTrainingVideo.mp4', title: 'TM-FLOW REPORT', description: ' TM-FLOW REPORT. VASCULAR FUNCTION ASSESSMENT. HR: 77. Height 5 ,. Weight 168 Lbs. BMI: 26.3. CLINICAL CONTEXT. Physician Name:.' }] }, { name: 'Software walkthrough', videodata: [{ url: 'https://betoparedesallvideos.s3.amazonaws.com/betoparedesallvideos/image-16375886325607774485510954222850.mp4', title: 'TM-FLOW REPORT', description: ' TM-FLOW REPORT. VASCULAR FUNCTION ASSESSMENT. HR: 77. Height 5 ,. Weight 168 Lbs. BMI: 26.3. CLINICAL CONTEXT. Physician Name:.' }, { url: 'https://betoparedesallvideos.s3.amazonaws.com/betoparedesallvideos/image-1637210549150PECEDoctorTrainingVideo.mp4', title: 'Software walkthrough', description: ' TM-FLOW REPORT. VASCULAR FUNCTION ASSESSMENT. HR: 77. Height 5 ,. Weight 168 Lbs. BMI: 26.3. CLINICAL CONTEXT. Physician Name:.' }] }];
+
+
+  public videolink: any = [];
+
   constructor(public api_service: ApiService,
     public activatedroute: ActivatedRoute, public snackBar: MatSnackBar) {
-      let endpoint = 'https://z2oo2a8oq9.execute-api.us-east-1.amazonaws.com/dev/api/marketiingreviewteach'
-      let data = {
-        productid: this.activatedroute.snapshot.params.product_id,
-        
-        userid: this.activatedroute.snapshot.params.rep_id
-      }
-      this.api_service.requestData(endpoint, data).subscribe((res: any) => {
-       console.log(res);
-       this.productdata=res.results.productdata[0];
-       this.techata=res.results.userdata[0];
-       console.log(this.productdata,'productdata',this.techata);
-       
-      })
+    let endpoint = 'https://z2oo2a8oq9.execute-api.us-east-1.amazonaws.com/dev/api/marketiingreviewteach'
+    let data = {
+      productid: this.activatedroute.snapshot.params.product_id,
+
+      userid: this.activatedroute.snapshot.params.rep_id
+    }
+    this.api_service.requestData(endpoint, data).subscribe((res: any) => {
+      console.log(res);
+      this.productdata = res.results.productdata[0];
+      this.techata = res.results.userdata[0];
+      console.log(this.productdata, 'productdata', this.techata);
+
+    })
   }
 
   ngOnInit() {
@@ -44,4 +50,18 @@ export class MarketingreviewNewComponent implements OnInit {
       });
     })
   }
+
+  videoplay(val) {
+
+    console.log(val);
+    this.videolink = val.videodata;
+
+
+
+
+
+  }
+
+
+
 }
