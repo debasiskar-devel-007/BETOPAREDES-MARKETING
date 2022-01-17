13db1cecee2c416d9fc18f518917c0d4de8d7b46
sourodipchowdhury <sourodip.influxiq@gmail.com>
2022-01-17 13:38:57 +0530

    meta service spec.ts fixed problem with proper import path and service class name

--- src/app/meta-service.service.spec.ts
+++ src/app/meta-service.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MetaServiceService } from './meta-service.service';
+// import { MetaServiceService } from '/srcmeta-service.service';
+import { MetaService } from './meta-service.service';
 
 describe('MetaServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: MetaServiceService = TestBed.get(MetaServiceService);
+    const service: MetaService = TestBed.get(MetaService);
     expect(service).toBeTruthy();
   });
 });
